f8802eb1b6bd041d90e6a84b4dec980e8e7e2e0b
Jan Lemmens <>
2026-01-02 14:38:54 +0100

    switched to simple http server

--- serve-spa.sh
+++ serve-spa.sh
@@ -3,14 +3,15 @@ set -eu
 
 cd "$(dirname "$0")/client/dist"
 python - <<'PY' &
+import SimpleHTTPServer
+import SocketServer
 import os
-from http.server import SimpleHTTPRequestHandler, HTTPServer
 
 PORT = 8000
 ROOT = os.getcwd()
 INDEX = os.path.join(ROOT, "index.html")
 
-class SPAHandler(SimpleHTTPRequestHandler):
+class SPAHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
   def do_GET(self):
     path = self.translate_path(self.path)
     if os.path.isdir(path):
@@ -18,14 +19,14 @@ class SPAHandler(SimpleHTTPRequestHandler):
         index_path = os.path.join(path, index)
         if os.path.exists(index_path):
           self.path = os.path.join(self.path, index)
-          return SimpleHTTPRequestHandler.do_GET(self)
+          return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
     if os.path.exists(path):
-      return SimpleHTTPRequestHandler.do_GET(self)
+      return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
     self.send_response(200)
     self.send_header("Content-type", "text/html")
     self.end_headers()
     with open(INDEX, "rb") as handle:
-      self.copyfile(handle, self.wfile)
+      self.wfile.write(handle.read())
 
   def do_HEAD(self):
     path = self.translate_path(self.path)
@@ -34,15 +35,15 @@ class SPAHandler(SimpleHTTPRequestHandler):
         index_path = os.path.join(path, index)
         if os.path.exists(index_path):
           self.path = os.path.join(self.path, index)
-          return SimpleHTTPRequestHandler.do_HEAD(self)
+          return SimpleHTTPServer.SimpleHTTPRequestHandler.do_HEAD(self)
     if os.path.exists(path):
-      return SimpleHTTPRequestHandler.do_HEAD(self)
+      return SimpleHTTPServer.SimpleHTTPRequestHandler.do_HEAD(self)
     self.send_response(200)
     self.send_header("Content-type", "text/html")
     self.end_headers()
 
-HTTPServer.allow_reuse_address = True
-httpd = HTTPServer(("", PORT), SPAHandler)
+SocketServer.TCPServer.allow_reuse_address = True
+httpd = SocketServer.TCPServer(("", PORT), SPAHandler)
 print("Serving SPA on http://localhost:%d" % PORT)
 httpd.serve_forever()
 PY
