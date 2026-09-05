8c6d9fb340557623f0a88052f4ae3c1a8cc6527d
Jan Lemmens <jan.lemmens@amexiogroup.com>
2026-09-05 09:59:09 +0200

    Remove MUI and emotion; shadcn/ui redesign complete

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,23 +1,18 @@
-import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { CssBaseline, ThemeProvider } from "@mui/material";
 import { App } from "./App";
-import { theme } from "./theme";
 import { AuthProvider } from "./auth/AuthContext";
 import { Toaster } from "@/components/ui/sonner";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-          <Toaster richColors position="top-right" />
-        </AuthProvider>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+        <Toaster richColors position="top-right" />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
